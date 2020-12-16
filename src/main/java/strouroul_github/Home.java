d5bd6c257b05eb49b24491192f7001822c68d2ba
Haytham Meiz <kool_egypt@hotmail.com>
2020-12-16 05:47:02 +0200

    WS SERVER
    Updated WS SERVER for home page

--- src/main/java/strouroul_github/Home.java
+++ src/main/java/strouroul_github/Home.java
@@ -41,7 +41,15 @@ public class Home extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-             out.println(request.getServletContext().getContextPath());
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Home</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Home at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
         }
     }
 
