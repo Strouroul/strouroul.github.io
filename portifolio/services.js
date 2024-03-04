ea3697220473d559f49e711d3443370aa9f663e6
Strouroul <kool_egypt@hotmail.com>
2024-03-04 02:33:07 +0200

    Adjusted portfolio

---
+++ portifolio/services.js
@@ -0,0 +1,23 @@
+let services_names={
+    "front_end":"Front-End Development",
+    "back_end":"Back-End Development",
+    "web_design":"Web Design"
+};
+
+let my_services={
+    "front_end_short": "Crafting captivating user experiences is my forte. As a front-end developer, I specialize in turning design visions into interactive and visually stunning websites. Proficient in HTML, CSS, and JavaScript, I ensure seamless navigation and responsiveness, enhancing the overall user journey.",
+
+    "front_end_long": "In my role as a front-end developer, I bring digital designs to life, focusing on the user interface and overall user experience. Proficient in HTML, CSS, and JavaScript, I specialize in creating responsive, visually appealing websites. By collaborating closely with design teams, I ensure the seamless integration of aesthetics and functionality, providing users with an engaging and intuitive online experience.",
+
+    "back_end_short": "As a backend enthusiast, I thrive in the realm of server-side logic and database management. My expertise lies in designing robust and scalable architecture to support the functionality of web applications. From crafting APIs to managing databases, I am dedicated to optimizing the backend for efficiency and reliability.",
+
+    "back_end_long": "Delving into the backend realm, I excel in developing the server-side logic and infrastructure that powers web applications. My proficiency extends to designing scalable architectures, crafting efficient APIs, and managing databases. With a keen eye for optimization, I ensure the backend operates seamlessly, supporting the overall performance and reliability of web applications.",
+
+    "back_end_short_2": "As a backend enthusiast, I thrive in the realm of server-side logic and database management. My expertise lies in designing robust and scalable architecture to support the functionality of web applications. From crafting APIs to managing databases, I am dedicated to optimizing the backend for efficiency and reliability.",
+
+    "back_end_long_2": "Navigating the intricate landscape of backend development, I excel in architecting the backbone of web applications. My expertise extends to designing and implementing scalable databases, optimizing server-side logic, and ensuring the seamless integration of various systems. From crafting robust APIs to handling data security, I am committed to building backend solutions that prioritize performance, reliability, and scalability. With a keen eye for efficiency, I continually explore emerging technologies to keep backend systems at the forefront of innovation, ensuring they meet the evolving demands of modern web applications.",
+
+    "web_design_short": "Designing digital experiences that captivate and inspire is my passion. As a web designer, I blend creativity with functionality, creating visually appealing layouts and graphics. Proficient in design tools and principles, I strive to elevate the aesthetic appeal of every web project.",
+
+    "web_design_long": "In the realm of web design, I am dedicated to merging creativity with functionality. Proficient in various design tools and principles, I specialize in crafting visually appealing layouts and graphics. My goal is to enhance the overall aesthetic and user engagement of web projects, ensuring a delightful and memorable online experience."
+}
