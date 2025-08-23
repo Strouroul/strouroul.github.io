22affd4e96ef9646464337f4469123d250c9b131
Strouroul <kool_egypt@hotmail.com>
2025-08-23 04:51:51 +0300

    Support R US
    moving the local stuff to /voip
    
    need to move all those leads files to /voip to avoid CSRF for helpdesk since github isnt ishopper !!

---
+++ SupportRUs/redirector.js
@@ -0,0 +1,13 @@
+let countdown_seconds=parseInt(document.getElementById('countdown_redirect').innerText)||0;
+const fullPath = window.location.pathname + window.location.search + window.location.hash;
+console.log(fullPath);
+
+setInterval(function(){
+    if(countdown_seconds===1){
+        window.location='https://www.ishopper.info'+fullPath;
+    }else{
+        countdown_seconds--;
+        document.getElementById('countdown_redirect').innerText=countdown_seconds;
+    }
+},1000)
+
